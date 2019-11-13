44cb130f494a66c346d5c0ba9b26cb8fc26336d0
gbrown <georg.braunbeck@googlemail.com>
2019-11-13 19:06:49 +0100

    added optical_polarization

---
+++ calcs/optical_polarization.py
@@ -0,0 +1,21 @@
+def calc_polarization(n):
+    polarization = 1 - 0.7 ** n
+    print('polarization: {:.4}'.format(polarization))
+    return polarization
+
+
+def percentage_of_polarized_states(polarization):
+    perc_of_polrzd_states = (1. + polarization) / 2.
+    print('percentage of polarized states: {:.4}'.format(perc_of_polrzd_states))
+
+
+def main():
+    n_cycles = 5
+
+    polarization = calc_polarization(n_cycles)
+
+    percentage_of_polarized_states(polarization)
+
+
+if __name__ == '__main__':
+    main()
