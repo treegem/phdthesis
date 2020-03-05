e78419e28a9079fa2b028d4408b8c2489969ea65
gbrown <georg.braunbeck@googlemail.com>
2020-03-05 16:36:24 +0100

    cw_odmr_spectrum

---
+++ images/chapter_4/cw_odmr.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sio
+
+from util import inches, smoothing
+
+
+def main():
+    cts, freqs = load_raw_data()
+
+    plot_and_save_odmr_spectrum(cts, freqs)
+
+
+def plot_and_save_odmr_spectrum(cts, freqs):
+    plt.figure(figsize=(inches.cm_to_inch(15), inches.cm_to_inch(5.5)))
+    plt.plot(freqs * 1e-9, smoothing.smooth_array_by_rolling_average(cts, 2), '.')
+    plt.xlabel('mw frequency (GHz)')
+    plt.ylabel('luminescence (cts/s)')
+    plt.tight_layout()
+    plt.savefig('cw_odmr_spectrum.png', dpi=500)
+
+
+def load_raw_data():
+    file_name_and_path = "//file/e24/Projects/ReinhardLab/data_setup_nv1/190709_D02C_AlOx/odmr.003.mat"
+    odmr_data = sio.loadmat(file_name_and_path)
+    cts = odmr_data['ci'][0]
+    freqs = np.linspace(odmr_data['fstart'][0][0], odmr_data['fend'][0][0], cts.shape[0])
+    return cts, freqs
+
+
+if __name__ == '__main__':
+    main()
---
+++ util/smoothing.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def smooth_array_by_rolling_average(array: np.ndarray, neighbors: int = 1):
+    smoothed_array = np.zeros_like(array)
+    for i, value in enumerate(array):
+        if i - neighbors < 0:
+            smoothed_array[i] = __smooth_single_value_by_rolling_average(array, i, neighbors=i)
+        elif i + neighbors >= array.shape[0]:
+            max_neighbors = array.shape[0] - i - 1
+            smoothed_array[i] = __smooth_single_value_by_rolling_average(array, i, neighbors=max_neighbors)
+        else:
+            smoothed_array[i] = __smooth_single_value_by_rolling_average(array, i, neighbors)
+    return smoothed_array
+
+
+def __smooth_single_value_by_rolling_average(array: np.ndarray, i: int, neighbors: int = 1):
+    summed_values = np.sum(array[i - neighbors:i + neighbors + 1])
+    return summed_values / (2 * neighbors + 1)
