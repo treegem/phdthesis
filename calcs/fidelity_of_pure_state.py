0217a18a01b41b7a8e161c38886c5f410a803fac
gbrown <georg.braunbeck@googlemail.com>
2020-04-24 18:07:56 +0200

    chapter 5 documentation of data

--- calcs/fidelity_of_pure_state.py
+++ calcs/fidelity_of_pure_state.py
@@ -5,7 +5,7 @@ def main():
     n_one_over_e = 130  # number of oscillations to reach 1/e
     phi = np.arccos(1 / np.e)
     phi_per_pi = phi / np.sqrt(2 * n_one_over_e)  # 2 because one oscillation is two pi pulses, sqrt wegen random walk
-    phi_per_pi = 2 * np.pi / 2**14 * np.sqrt(25)
+    # phi_per_pi = 2 * np.pi / 2**14 * np.sqrt(25)
 
     a_0 = np.cos(phi_per_pi)  # factor of |0>, a_1 not needed for calculation
     fidelity = np.power(a_0, 2)
