19e0626994b1f016be79926154f0990ddeb29d2d
gbrown <georg.braunbeck@googlemail.com>
2020-05-10 20:03:54 +0200

    chapter 5 beam comparison

---
+++ images/chapter_5/beam_comparison.py
@@ -0,0 +1,79 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from util.inches import cm_to_inch
+from util.tum_jet import tum_jet
+
+
+class BeamComparison:
+    def __init__(self):
+        self.gaussian_data = self.__load_data('proem_009.txt')
+        self.square_data = self.__load_data('proem_010.txt')
+        self.fig = None
+        self.axes = (None, None)
+
+    @staticmethod
+    def __load_data(filename):
+        return np.loadtxt(
+            os.path.join('//file/e24/Projects/ReinhardLab/data_setup_nv1/170306_phase_plates_rabi_echo_pro', filename))
+
+    def plot(self):
+        self.__create_and_scale_figure_and_axes()
+        square_image = self.__imshow_both_axes()  # image necessary to determine colorbar range
+        self.__add_colorbar(square_image)
+        plt.savefig('beam_comparison.png', dpi=500)
+
+    def __create_and_scale_figure_and_axes(self):
+        self.fig, self.axes = plt.subplots(nrows=1, ncols=2)
+        self.fig.set_figheight(cm_to_inch(8))
+        self.fig.set_figwidth(cm_to_inch(15))
+
+    def __imshow_both_axes(self):
+        self.__imshow_single_axis(0, self.gaussian_data)
+        square_image = self.__imshow_single_axis(1, self.square_data)
+        self.__set_labels()
+        self.__set_y_ticks_and_labels()
+        return square_image
+
+    def __set_y_ticks_and_labels(self):
+        self.axes[0].set_yticks([0, 5, 10, 15])
+        self.axes[1].set_yticks([0, 5, 10, 15])
+        self.axes[1].set_yticklabels(['', '', '', ''])
+
+    def __imshow_single_axis(self, axis_index, data):
+        vmin, vmax = self.__define_min_max_for_plot()
+        return self.axes[axis_index].imshow(data, vmin=vmin, vmax=vmax, cmap=tum_jet, origin='lower', aspect=1,
+                                            interpolation='bilinear', extent=self.__convert_extent_from_pixels_to_um())
+
+    def __define_min_max_for_plot(self):
+        gaussian_min = self.gaussian_data.min()
+        gaussian_max = self.gaussian_data.max()
+        square_min = self.gaussian_data.min()
+        square_max = self.gaussian_data.max()
+        return min(gaussian_min, square_min), max(gaussian_max, square_max)
+
+    def __convert_extent_from_pixels_to_um(self):
+        return [0, self.__convert_pixels_to_um(200), 0, self.__convert_pixels_to_um(250)]
+
+    @staticmethod
+    def __convert_pixels_to_um(pixels):
+        um_per_pixels = 10 / 150
+        return pixels * um_per_pixels
+
+    def __set_labels(self):
+        self.axes[0].set_xlabel(r'$x$ ($\mu$m)')
+        self.axes[0].set_ylabel(r'$y$ ($\mu$m)')
+        self.axes[1].set_xlabel(r'$x$ ($\mu$m)')
+
+    def __add_colorbar(self, square_image):
+        bottom, top, right = 0.15, 0.9, 0.83
+        self.fig.subplots_adjust(bottom=bottom, top=0.9, left=0.05, right=right, wspace=-0.1)
+        cbar_ax = self.fig.add_axes([right + 0.01, bottom, 0.05, top - bottom])
+        self.fig.colorbar(square_image, cax=cbar_ax, label=r'luminescence (cts/s)')
+
+
+if __name__ == '__main__':
+    beam_comparison = BeamComparison()
+    beam_comparison.plot()
