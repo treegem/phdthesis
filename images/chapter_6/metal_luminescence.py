39bbb51eab98a2a915e9efa1f8d315d1f4169399
gbrown <georg.braunbeck@googlemail.com>
2020-12-23 14:22:25 +0100

    chapter6/spin_properties

--- images/chapter_6/metal_luminescence.py
+++ images/chapter_6/metal_luminescence.py
@@ -1,3 +1,5 @@
+#  adapted code from Niklas
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,35 +8,38 @@ import scipy.io
 from matplotlib_scalebar.scalebar import ScaleBar
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy import ndimage as ndi
+from scipy.optimize import curve_fit
 from skimage.feature import peak_local_max
 from skimage.filters import gaussian
 from skimage.morphology import flood
 from skimage.segmentation import watershed
 from skimage.transform import resize
 
+from util.inches import cm_to_inch
 from util.tum_jet import tum_jet
 
 
 class MetalLuminescencePlotter:
 
+    def __init__(self):
+        mpl.rcParams['text.latex.preamble'] = [r'\usepackage{amsmath}']
+        self.min_v = 0.00e2
+        self.max_v = 1.4e2
+        self.map_c = tum_jet
+
     def plot(self):
-        title_fontsize = 7.5
-        minv = 0.00e2
-        maxv = 1.4e2
-        mapc = tum_jet
         title_pad = 3
-        intp = 'gaussian'
+        interpolation = 'gaussian'
         fig, axs = plt.subplots(3, 3, gridspec_kw={'width_ratios': [1, 1, 0.2], 'wspace': 0.10, 'hspace': 0.3},
-                                figsize=(3.37, 4))
-        fig.tight_layout()
+                                figsize=(cm_to_inch(15) * 0.8, cm_to_inch(14)))
         plt.sca(axs[0, 0])
         nicer_ax2(plt.gca())
-        DNi = r'D$_\text{Ni}$'
-        DPd = r'D$_\text{Pd}$'
+        d_ni = r'D$_\text{Ni}$'
+        d_pd = r'D$_\text{Pd}$'
 
-        DUV = r'D$_\text{UV}$'
-        DAlOx = r'D$_\text{AlOx}$'
-        plt.title(DNi, fontsize=title_fontsize, pad=title_pad)
+        d_uv = r'D$_\text{UV}$'
+        d_al_ox = r'D$_\text{AlOx}$'
+        plt.title(d_ni, fontsize=8, pad=title_pad)
         plt.tick_params(direction='inout', left=False, bottom=False)
         plt.xlim(0.5, 20)
         plt.ylim(0, 19.5)
@@ -60,49 +65,46 @@ class MetalLuminescencePlotter:
         x02c = mat['x'][0]
         y02c = mat['y'][0]
         z02c = mat['result'] / 1e3
-        filepath = 'metal_luminescence/2019-07-10_D01C1200/scan.016.mat'
-        mat = scipy.io.loadmat(filepath)
-        plt.imshow(z45.copy(), vmin=minv, vmax=maxv, cmap=mapc, interpolation=intp,
+        plt.imshow(z45.copy(), vmin=self.min_v, vmax=self.max_v, cmap=self.map_c, interpolation=interpolation,
                    extent=[0, x45[-1] - x45[0], 0, y45[-1] - y45[0]])
-        print(len(x45) / (x45[-1] - x45[0]), x45[-1] - x45[0])
         scalebar45 = ScaleBar(1E-6)
         plt.gca().add_artist(scalebar45)
 
         plt.sca(axs[0, 1])
         nicer_ax2(plt.gca())
-        plt.title(DPd, fontsize=title_fontsize, pad=title_pad)
+        plt.title(d_pd, fontsize=8, pad=title_pad)
         plt.tick_params(direction='inout', right=True, top=True)
         plt.xlim(0.5, 20)
         plt.ylim(0, 19.5)
         plt.gca().yaxis.set_ticks([])
         plt.gca().xaxis.set_ticks([])
-        plt.imshow(z44, vmin=minv, vmax=maxv, cmap=mapc, interpolation=intp,
+        plt.imshow(z44, vmin=self.min_v, vmax=self.max_v, cmap=self.map_c, interpolation=interpolation,
                    extent=[0, x44[-1] - x44[0], 0, y44[-1] - y44[0]])
         scalebar44 = ScaleBar(1E-6)
         plt.gca().add_artist(scalebar44)
 
         plt.sca(axs[1, 0])
         nicer_ax2(plt.gca())
-        plt.title(DUV, fontsize=title_fontsize, pad=title_pad)
+        plt.title(d_uv, fontsize=8, pad=title_pad)
         plt.tick_params(direction='inout', left=True, bottom=True)
         plt.xlim(0, 19.5)
         plt.ylim(0, 19.5)
         plt.gca().yaxis.set_ticks([])
         plt.gca().xaxis.set_ticks([])
-        plt.imshow(z01b, vmin=minv, vmax=maxv, cmap=mapc, interpolation=intp,
+        plt.imshow(z01b, vmin=self.min_v, vmax=self.max_v, cmap=self.map_c, interpolation=interpolation,
                    extent=[0, x01b[-1] - x01b[0], 0, y01b[-1] - y01b[0]])
         scalebar01b = ScaleBar(1E-6)
         plt.gca().add_artist(scalebar01b)
 
         plt.sca(axs[1, 1])
         nicer_ax2(plt.gca())
-        plt.title(DAlOx, fontsize=title_fontsize, pad=title_pad)
+        plt.title(d_al_ox, fontsize=8, pad=title_pad)
         plt.tick_params(direction='inout', right=True, top=True)
         plt.xlim(0.5, 20)
         plt.ylim(0, 19.5)
         plt.gca().yaxis.set_ticks([])
         plt.gca().xaxis.set_ticks([])
-        plt.imshow(z02c, vmin=minv, vmax=maxv, cmap=mapc, interpolation=intp,
+        plt.imshow(z02c, vmin=self.min_v, vmax=self.max_v, cmap=self.map_c, interpolation=interpolation,
                    extent=[0, x02c[-1] - x02c[0], 0, y02c[-1] - y02c[0]])
         scalebar02c = ScaleBar(1E-6)
         plt.gca().add_artist(scalebar02c)
@@ -115,7 +117,7 @@ class MetalLuminescencePlotter:
         areas = [(4, 4), (4, 4), (4, 4), (4, 4)]
         hist_dat_mat = []
         for mat, title, threshold, area in zip(mats, titles, thresholds, areas):
-            hist_dat_mat.append(find_peaks_simple(mat, threshold=threshold, area=area, sigma=1.5, title=title))
+            hist_dat_mat.append(find_peaks_simple(mat, threshold=threshold, area=area, sigma=1.5))
         wid = np.mean(hist_dat_mat[imat][0][:-1] - hist_dat_mat[imat][0][1:])
         plt.bar(hist_dat_mat[imat][0], hist_dat_mat[imat][1], wid, color='xkcd:gray')
         plt.plot(hist_dat_mat[imat][0], hist_dat_mat[imat][2], lw=0.7, color='xkcd:black')
@@ -132,15 +134,14 @@ class MetalLuminescencePlotter:
 
         plt.annotate('individual', xy=(74, 33), xytext=(110, 38.5), fontsize=6, ha='left', va='center')
         plt.annotate("", xy=(74, 36), xytext=(110, 39), arrowprops=dict(arrowstyle="->"), fontsize=6)
-        plt.title(DPd, fontsize=title_fontsize, pad=0)
+        plt.title(d_pd, fontsize=8, pad=0)
         plt.sca(axs[2, 1])
 
         yield_val = [5.612, 5.016, 3.054, 2.888]
         brightness_val = [hist_dat_mat[i][5] for i in range(len(hist_dat_mat))]
         brightness_sigma = [hist_dat_mat[i][6] for i in range(len(hist_dat_mat))]
         yield_error = [0.423, 0.385, 0.248, 0.234]
-        plt.errorbar(yield_val, brightness_val, xerr=yield_error, yerr=brightness_sigma, fmt='s', ms=2, lw=1,
-                     color='k')  # c = [colors[0], colors[1], colors[2], colors[3], colors[5]])
+        plt.errorbar(yield_val, brightness_val, xerr=yield_error, yerr=brightness_sigma, fmt='s', ms=2, lw=1, color='k')
         plt.annotate('Ni', (yield_val[0], brightness_val[0]), xytext=(yield_val[0] - 0.07, brightness_val[0] + 2),
                      ha='right', fontsize=6, va='bottom')
         plt.annotate('Pd', (yield_val[1], brightness_val[1]), xytext=(yield_val[1] + 0.1, brightness_val[1] - 4),
@@ -177,7 +178,7 @@ class MetalLuminescencePlotter:
         plt.sca(axcolorbar)
         divider = make_axes_locatable(axcolorbar)
         cax1 = divider.append_axes("left", size="80%", pad=0.00)
-        norm = mpl.colors.Normalize(vmin=minv, vmax=maxv)
+        norm = mpl.colors.Normalize(vmin=self.min_v, vmax=self.max_v)
         mpl.colorbar.ColorbarBase(cax1, cmap=tum_jet,
                                   norm=norm,
                                   orientation='vertical',
@@ -194,16 +195,14 @@ class MetalLuminescencePlotter:
         plt.rc('scalebar', location='lower right')
         plt.rc('scalebar', color='w')
 
-        plt.figtext(0.165, 0.895, 'a', fontsize=8)
-        plt.figtext(0.52, 0.895, 'b', fontsize=8)
-        plt.figtext(0.165, 0.623, 'c', fontsize=8)
-        plt.figtext(0.52, 0.623, 'd', fontsize=8)
-        plt.figtext(0.165, 0.345, 'e', fontsize=8)
-        plt.figtext(0.52, 0.345, 'f', fontsize=8)
+        plt.figtext(0.165, 0.895, 'a)', fontsize=8)
+        plt.figtext(0.52, 0.895, 'b)', fontsize=8)
+        plt.figtext(0.165, 0.623, 'c)', fontsize=8)
+        plt.figtext(0.52, 0.623, 'd)', fontsize=8)
+        plt.figtext(0.165, 0.345, 'e)', fontsize=8)
+        plt.figtext(0.52, 0.345, 'f)', fontsize=8)
 
-        fig.savefig('metal_luminescence' + '/confocal_pic.pdf', dpi=600, pad_inches=0, bbox_inches='tight')
-
-        plt.show()
+        fig.savefig('metal_luminescence' + '/confocal_pic.jpg', dpi=600, pad_inches=0, bbox_inches='tight')
 
 
 def nicer_ax2(ax):
@@ -216,98 +215,56 @@ def nicer_ax2(ax):
     ax.yaxis.tick_left()
 
 
-def find_peaks_simple(matIn, threshold=5, area=(4, 4), sigma=1, title='None',
-                      borders=[0.305, 1.33489786, 1.42829031, 1.80438237, 2.79750851]):
-    local_max, local_max_val, marks, ws_area, flood_sections, flood_values = get_flood_array(matIn, threshold, area)
+def find_peaks_simple(mat_in, threshold=5., area=(4, 4), sigma=1., borders=None):
+    if borders is None:
+        borders = [0.305, 1.33489786, 1.42829031, 1.80438237, 2.79750851]
+    local_max, local_max_val, marks, ws_area, flood_sections, flood_values = get_flood_array(mat_in, threshold, area)
 
     resize_factor = 3
-    upsampled = resize(matIn.copy(), np.array(matIn.shape) * resize_factor, order=0)
+    upsampled = resize(mat_in.copy(), np.array(mat_in.shape) * resize_factor, order=0)
     upsampled = gaussian(upsampled, sigma=sigma, cval=0)
     local_max_up, local_max_val_up, marks_up, ws_area_up, flood_sections_up, flood_values_up = get_flood_array(
         upsampled, threshold, np.array(area) * resize_factor)
 
-    fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(12 * 2, 4.5 * 2), sharex=False, sharey=False, )
-    ax = axes.ravel()
-
     bins = 50
-    values, binedges = np.histogram(local_max_val, bins=bins, density=False)
-    bin_mid = (binedges[1:] + binedges[:-1]) / 2
-    singleValue = bin_mid[np.argmax(values)]
-
-    values, binedges = np.histogram(local_max_val / singleValue, bins=bins, density=False, range=(0, 3))
-    bin_mid = (binedges[1:] + binedges[:-1]) / 2
+    values, bin_edges = np.histogram(local_max_val, bins=bins, density=False)
+    bin_mid = (bin_edges[1:] + bin_edges[:-1]) / 2
+    single_value = bin_mid[np.argmax(values)]
 
     values_up, binedges_up = np.histogram(local_max_val_up, bins=120, density=False)
     bin_mid_up = (binedges_up[1:] + binedges_up[:-1]) / 2
-    singleValue_up = bin_mid_up[np.argmax(values_up)]
+    single_value_up = bin_mid_up[np.argmax(values_up)]
 
-    values_up, binedges_up = np.histogram(local_max_val_up / singleValue_up, bins=bins, density=False, range=(0, 3))
+    values_up, binedges_up = np.histogram(local_max_val_up / single_value_up, bins=bins, density=False, range=(0, 3))
     bin_mid_up = (binedges_up[1:] + binedges_up[:-1]) / 2
 
     p0 = [0.1, 1, 0.4, 0.21, 1.7, 0.5]
     bounds = ([0, 0, 0, 0, 1.3, .38], [np.inf, 1.3, 60, np.inf, np.inf, 20])
-    #     bounds = ([0,0,0,0,1.3,.38],[np.inf,1.3,5,np.inf,np.inf,5])
     try:
         coeff, b = curve_fit(double_peak, bin_mid_up, values_up, p0=p0, bounds=bounds)
     except:
         coeff = p0
 
-    coeffPd = pd.DataFrame()
-    countRange = []
-    countRange_up = []
-    #     1,1.43,1.629
-    #     print('border', borders)
+    coeff_pd = pd.DataFrame()
+    count_range = []
+    count_range_up = []
     for ni, (i, j) in enumerate(
             [(borders[0], borders[1]), (borders[1], borders[2]), (borders[2], borders[3]), (borders[3], borders[4]),
              (borders[4], 1000)]):
-        #         print(ni, i,j)
-        count_mask = (local_max_val > (i) * singleValue) & (local_max_val <= (j) * singleValue).astype('int')
-        count_mask_up = (local_max_val_up > (i) * singleValue_up) & (local_max_val_up <= (j) * singleValue_up).astype(
+        count_mask = (local_max_val > (i) * single_value) & (local_max_val <= (j) * single_value).astype('int')
+        count_mask_up = (local_max_val_up > (i) * single_value_up) & (local_max_val_up <= (j) * single_value_up).astype(
             'int')
-        #         print(count_mask)
-        countRange.append(np.count_nonzero(count_mask) * (ni + 1))
-        countRange_up.append(np.count_nonzero(count_mask_up) * (ni + 1))
-    coeffPd['CountRange'] = countRange
-    coeffPd['CountRange_up'] = countRange_up
-    # display(coeffPd)
-
-    fig.suptitle(title)
-    local_maxi_indices = np.array(np.argwhere(local_max))
-    ax[0].imshow(matIn, vmax=4 * threshold)
-    ax[0].scatter(local_maxi_indices[:, 1], local_maxi_indices[:, 0], color='r', s=0.3)
-    ax[1].set_title('Maxima')
-    ax[1].imshow(local_max)
-    ax[2].imshow(flood_sections, cmap='jet')
-    ax[3].bar(bin_mid, values, width=0.03, align='center')
-    #     ax[4].hist(flood_values, bins = bins, range = (0, 200* singleValue))
-
-    local_maxi_Indices_up = np.array(np.argwhere(local_max_up))
-    ax[4].imshow(upsampled, vmax=4 * threshold)
-    ax[4].scatter(local_maxi_Indices_up[:, 1], local_maxi_Indices_up[:, 0], color='r', s=0.3)
-    ax[5].set_title('Maxima')
-    ax[5].imshow(local_max_up, )
-    ax[6].imshow(flood_sections_up, cmap='jet')
-    ax[7].bar(bin_mid_up, values_up, width=0.03, align='center')
-    #     ax[7].hist(local_max_val_up / singleValue_up, bins = bins, range = (0,3))
-    ax[7].axvline(borders[0], lw=0.3, c='r')
-    ax[7].axvline(borders[1], lw=0.3, c='r')
-    ax[7].axvline(borders[2], lw=0.3, c='r')
-    ax[7].axvline(borders[3], lw=0.3, c='r')
-    ax[7].axvline(borders[4], lw=0.3, c='r')
+        count_range.append(np.count_nonzero(count_mask) * (ni + 1))
+        count_range_up.append(np.count_nonzero(count_mask_up) * (ni + 1))
+    coeff_pd['CountRange'] = count_range
+    coeff_pd['CountRange_up'] = count_range_up
 
     rng = bin_mid_up
-    peakD = double_peak(rng, *coeff)
-    ax[7].plot(rng, peakD, 'k')
+    peak_d = double_peak(rng, *coeff)
     peak1, peak2 = double_peak_ind(rng, *coeff)
-    ax[7].plot(rng, peak1, ':', color='xkcd:light gray')
-    ax[7].plot(rng, peak2, ':', color='xkcd:light gray')
-    #     ax[9].hist(flood_values_up, bins = bins, range = (0, 200 * singleValue_up))
 
-    plt.savefig('metal_luminescence' + '/simple_' + title + '.jpg')
-    plt.show()
-
-    return bin_mid_up * singleValue_up, values_up, peakD, peak1, peak2, coeff[1] * singleValue_up, coeff[
-        2] * singleValue_up / 2, len(local_max_val_up), len(local_max_val)
+    return bin_mid_up * single_value_up, values_up, peak_d, peak1, peak2, coeff[1] * single_value_up, coeff[
+        2] * single_value_up / 2, len(local_max_val_up), len(local_max_val)
 
 
 def nicer_ax(ax):
---
+++ images/chapter_6/spin_properties.py
@@ -0,0 +1,192 @@
+#  adapted code from Niklas
+
+import os
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+import pandas as pd
+import scipy.io
+import seaborn as sns
+
+from util.inches import cm_to_inch
+
+
+def load_t2_data(file_path: str):
+    data_dir = os.path.join('spin_properties', file_path) + 'T2.mat'
+    mat = scipy.io.loadmat(data_dir)
+    t2_data = mat['csvDat']
+    return t2_data
+
+
+def load_odmr_data(file_path: str):
+    data_dir = os.path.join('spin_properties', file_path) + 'ODMR.mat'
+    mat = scipy.io.loadmat(data_dir)
+    odmrh_data = mat['relODMRH'][0]
+    odmrwid_data = mat['ODMRwid'][0] / np.sqrt(0.5 * (1 + 1.26 ** 2))
+    return odmrh_data, odmrwid_data
+
+
+class SpinPropertiesPlotter:
+
+    def __init__(self):
+        mpl.rcParams['text.latex.preamble'] = [r'\usepackage{amsmath}']
+
+    def plot(self):
+        DNi = r'D$_\text{Ni}$'
+        DPd = r'D$_\text{Pd}$'
+        DUV = r'D$_\text{UV}$'
+        DAlOx = r'D$_\text{AlOx}$'
+
+        csvDatD45_2 = load_t2_data('2019-06-06D45/')
+        csvDatD01B_1 = load_t2_data('2019-06-08/')
+        csvDatD44_1 = load_t2_data('2019-06-12/')
+        csvDatD02C = load_t2_data('2019-07-9/')
+
+        spin_property_fig, [[ODMRAmpax, ODMRwidax], [T2Ax, boxAx]] = \
+            plt.subplots(2, 2, gridspec_kw={'width_ratios': [1, 1], 'wspace': 0.2, 'hspace': 0.4},
+                         figsize=(cm_to_inch(15), cm_to_inch(10)))
+
+        nicer_ax(boxAx)
+        nicer_ax(T2Ax)
+        nicer_ax(ODMRAmpax)
+        nicer_ax(ODMRwidax)
+
+        T2Ax.set_xlim(0, 101)
+        ODMRAmpax.set_xlim(0, 101)
+        ODMRwidax.set_xlim(0, 101)
+
+        boxAx.set_ylim(-3, 105)
+        boxAx.tick_params(axis=u'x', which=u'both', length=0)
+
+        n = pd.DataFrame({'Probe': np.repeat(DNi, csvDatD45_2.shape[0]), 'Fit': np.repeat('Var', csvDatD45_2.shape[0]),
+                          'T2': csvDatD45_2[:, 12]})
+        o = pd.DataFrame(
+            {'Probe': np.repeat(DUV, csvDatD01B_1.shape[0]), 'Fit': np.repeat('Var', csvDatD01B_1.shape[0]),
+             'T2': csvDatD01B_1[:, 12]})
+        p = pd.DataFrame({'Probe': np.repeat(DPd, csvDatD44_1.shape[0]), 'Fit': np.repeat('Var', csvDatD44_1.shape[0]),
+                          'T2': csvDatD44_1[:, 12]})
+        q = pd.DataFrame({'Probe': np.repeat(DAlOx, csvDatD02C.shape[0]), 'Fit': np.repeat('Var', csvDatD02C.shape[0]),
+                          'T2': csvDatD02C[:, 12]})
+
+        df = pd.concat([n, p, o, q])
+
+        colors = [(0, 101 / 256, 189 / 256),  # TUM Blue
+                  (145 / 256, 172 / 256, 107 / 256),  # TUM Green
+                  (255 / 256, 128 / 256, 0),  # TUM Orange
+                  (229 / 256, 52 / 256, 24 / 256),  # TUM Red
+                  "#884EA1",
+                  (156 / 255, 157 / 255, 159 / 255),  # TUM Yellow
+                  ]
+
+        sns.stripplot(x='Probe', y='T2', data=df, palette=colors, jitter=0.2, dodge=False, size=1.5, alpha=1,
+                      linewidth=0,
+                      ax=boxAx)
+
+        sns.boxplot(x='Probe', y='T2', data=df, palette=[(0, 0, 0, 0.75)],
+                    boxprops={'linewidth': 0.5, 'color': [0.8] * 3, 'edgecolor': 'k', 'alpha': 0.7},
+                    medianprops={'linewidth': 1, 'color': 'k'}, meanprops={'linestyle': '-', "color": "xkcd:dark gray"},
+                    meanline=False, showmeans=False, showfliers=False, saturation=1, ax=boxAx, linewidth=0.7)
+
+        plt.legend().remove()
+        plt.legend().remove()
+        boxAx.set_xlabel('')
+        boxAx.set_ylabel(r'$T_2$ [\textmu s]')
+        boxAx.yaxis.set_label_position("right")
+        boxAx.yaxis.tick_right()
+
+        T2SortD44 = csvDatD44_1[csvDatD44_1[:, 12].argsort()]
+        T2SortD45 = csvDatD45_2[csvDatD45_2[:, 12].argsort()]
+        T2SortD01B = csvDatD01B_1[csvDatD01B_1[:, 12].argsort()]
+        T2SortD02C = csvDatD02C[csvDatD02C[:, 12].argsort()]
+        scat_s = 1
+        T2Ax.scatter(np.linspace(0, 100, T2SortD45.shape[0]), T2SortD45[:, 12], s=scat_s, color=colors[0], label=DNi)
+        T2Ax.scatter(np.linspace(0, 100, T2SortD44.shape[0]), T2SortD44[:, 12], s=scat_s, color=colors[1], label=DPd)
+        T2Ax.scatter(np.linspace(0, 100, T2SortD01B.shape[0]), T2SortD01B[:, 12], s=scat_s, color=colors[2], label=DUV)
+        T2Ax.scatter(np.linspace(0, 100, T2SortD02C.shape[0]), T2SortD02C[:, 12], s=scat_s, color=colors[3],
+                     label=DAlOx)
+
+        lw = 0.5
+        T2Ax.axvline(50, color='xkcd:gray', ls='--', linewidth=lw)
+        T2Ax.axvline(75, color='xkcd:gray', ls=':', linewidth=lw)
+        T2Ax.axvline(25, color='xkcd:gray', ls=':', linewidth=lw)
+        T2Ax.axvline(100, color='xkcd:gray', ls=(0, (1, 3)), linewidth=lw)
+        T2Ax.set_ylabel(r'$T_2$ [\textmu s]')
+        T2Ax.xaxis.set_major_locator(ticker.MultipleLocator(25.00))
+        T2Ax.xaxis.set_major_formatter(ticker.FormatStrFormatter(r"$%d\%%$"))
+        T2Ax.set_ylim(-5, 180)
+
+        plt.sca(ODMRAmpax)
+        plt.ylabel(r'$A_\mathrm{rel}$ [\%]')
+
+        ODMRH_45, ODMRWid_45 = load_odmr_data('2019-06-06D45/')
+        ODMRH_44, ODMRWid_44 = load_odmr_data('2019-06-12/')
+        ODMRH_01b, ODMRWid_01b = load_odmr_data('2019-06-08/')
+        ODMRH_02c, ODMRWid_02c = load_odmr_data('2019-07-9/')
+
+        SortODMRH_45 = ODMRH_45[ODMRH_45.argsort()] * 1E2
+        SortODMRH_44 = ODMRH_44[ODMRH_44.argsort()] * 1E2
+        SortODMRH_01b = ODMRH_01b[ODMRH_01b.argsort()] * 1E2
+        SortODMRH_02c = ODMRH_02c[ODMRH_02c.argsort()] * 1E2
+
+        plt.scatter(np.linspace(0, 100, ODMRH_45.shape[0]), SortODMRH_45, s=scat_s, color=colors[0])
+        plt.scatter(np.linspace(0, 100, ODMRH_44.shape[0]), SortODMRH_44, s=scat_s, color=colors[1])
+        plt.scatter(np.linspace(0, 100, ODMRH_01b.shape[0]), SortODMRH_01b, s=scat_s, color=colors[2])
+        plt.scatter(np.linspace(0, 100, ODMRH_02c.shape[0]), SortODMRH_02c, s=scat_s, color=colors[3])
+
+        plt.axvline(50, color='xkcd:gray', ls='--', linewidth=lw)
+        plt.axvline(75, color='xkcd:gray', ls=':', linewidth=lw)
+        plt.axvline(25, color='xkcd:gray', ls=':', linewidth=lw)
+        plt.axvline(100, color='xkcd:gray', ls=(0, (1, 3)), linewidth=lw)
+
+        plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(25.00))
+        plt.gca().xaxis.set_major_formatter(ticker.FormatStrFormatter(r"$%d\%%$"))
+
+        plt.sca(ODMRwidax)
+
+        plt.ylabel(r'$\Gamma$ [MHz]')
+        SortODMRWid_45 = ODMRWid_45[ODMRWid_45.argsort()] * 1E3
+        SortODMRWid_44 = ODMRWid_44[ODMRWid_44.argsort()] * 1E3
+        SortODMRWid_01b = ODMRWid_01b[ODMRWid_01b.argsort()] * 1E3
+        SortODMRWid_02c = ODMRWid_02c[ODMRWid_02c.argsort()] * 1E3
+
+        plt.scatter(np.linspace(0, 100, ODMRWid_45.shape[0]), SortODMRWid_45, s=scat_s, color=colors[0], label=DNi)
+        plt.scatter(np.linspace(0, 100, ODMRWid_44.shape[0]), SortODMRWid_44, s=scat_s, color=colors[1], label=DPd)
+        plt.scatter(np.linspace(0, 100, ODMRWid_01b.shape[0]), SortODMRWid_01b, s=scat_s, color=colors[2], label=DUV)
+        plt.scatter(np.linspace(0, 100, ODMRWid_02c.shape[0]), SortODMRWid_02c, s=scat_s, color=colors[3], label=DAlOx)
+
+        plt.legend(markerscale=2)
+
+        plt.axvline(50, color='xkcd:gray', ls='--', linewidth=lw)
+        plt.axvline(75, color='xkcd:gray', ls=':', linewidth=lw)
+        plt.axvline(25, color='xkcd:gray', ls=':', linewidth=lw)
+        plt.axvline(100, color='xkcd:gray', ls=(0, (1, 3)), linewidth=lw)
+
+        plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(25.00))
+        plt.gca().xaxis.set_major_formatter(ticker.FormatStrFormatter(r"$%d\%%$"))
+        plt.gca().yaxis.set_label_position("right")
+        plt.gca().yaxis.tick_right()
+
+        plt.figtext(0.02, 0.9, 'a)', fontsize=8)
+        plt.figtext(0.55, 0.9, 'b)', fontsize=8)
+        plt.figtext(0.02, 0.455, 'c)', fontsize=8)
+        plt.figtext(0.55, 0.455, 'd)', fontsize=8)
+
+        plt.tight_layout(pad=0.1, w_pad=2, h_pad=2)
+        spin_property_fig.savefig('spin_properties/spin_properties.png', dpi=600, pad_inches=0.1, bbox_inches='tight')
+
+
+def nicer_ax(ax):
+    ax.spines['top'].set_visible(True)
+    ax.spines['right'].set_visible(True)
+    ax.spines['left'].set_linewidth(0.8)
+    ax.spines['bottom'].set_linewidth(0.8)
+    ax.spines['left'].set_linewidth(0.8)
+    ax.spines['bottom'].set_linewidth(0.8)
+    ax.yaxis.tick_left()
+
+
+if __name__ == '__main__':
+    plotter = SpinPropertiesPlotter()
+    plotter.plot()
